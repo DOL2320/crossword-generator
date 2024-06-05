6307174fab509dfeb2e4319740e42810a2111b42
DOL2320 <dorian.majano@gmail.com>
2024-06-05 13:52:11 -0700

    Setup controller

--- application/src/main/java/com/crossword/controller/CrosswordPuzzleController.java
+++ application/src/main/java/com/crossword/controller/CrosswordPuzzleController.java
@@ -1,4 +1,8 @@
 package com.crossword.controller;
 
+import com.crossword.mapper.CrosswordPuzzleRepresentationMapper;
+
 public class CrosswordPuzzleController {
+
+  private CrosswordPuzzleRepresentationMapper crosswordPuzzleRepresentationMapper;
 }
